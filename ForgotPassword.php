5c081b4b4f1a062459c35e0e6f7835e30e47c858
meganmbi <meganbinns9@icloud.com>
2025-01-05 03:13:59 +0000

    Create ForgotPassword.php

---
+++ ForgotPassword.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Metadata -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- LINKS
+         Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"rel="nofollow" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- External CSS -->
+    <link rel="stylesheet" href="public_html/style/Bookrooms.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- Javascript -->
+    <script src="public_html/js/Bookrooms.js"></script>
+
+
+    <title>Forgot Password </title>
+</head>
