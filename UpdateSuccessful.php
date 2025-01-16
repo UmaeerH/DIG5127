88c5d48a03292b2bae9225d1b804beeb6b64d29a
meganmbi <meganbinns9@icloud.com>
2025-01-16 00:01:34 +0000

    UpdateSuccessful.php

---
+++ UpdateSuccessful.php
@@ -0,0 +1,35 @@
+<php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Metadata -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- LINKS
+         Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"rel="nofollow" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- External CSS -->
+    <link rel="stylesheet" href="public_html/style/UpdateSucessfulPage.css">
+    <!-- Javascript -->
+    <script src="public_html/js/UpdateSuccessful.js"></script>
+
+
+    <title> Update Successful Page </title>
+</head>
+
+
+<h1> Update Successful </h1>
+<h1> FROM </h1>
+<div class="time-box">
+    <div class="icon">C</div>
+    <img src="parkside-gallery.jpeg" alt="Location image">
+</div>
+
+
+
+    
+                <h1> TO </h1> 
+
+
