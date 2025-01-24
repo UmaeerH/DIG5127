3159883ea6779b7419401ed040cb12db91242c7e
UmaeerH <umaeerh04@gmail.com>
2025-01-24 15:19:08 +0000

    Revert "Update RoomSelection.php"
    This reverts commit 291c44535ea7f36b0fa22896292a7609d9d404f6.

--- RoomSelection.php
+++ RoomSelection.php
@@ -1,10 +1,32 @@
 <?php
-session_start();
+session_start(); // Ensure session is started
 
 include "resources/database.php";
+include 'resources/html_construct.php';
 
 if ($conn === null) {
     die("Database connection not established.");
+}
+
+// Ensure 'selected_date' is set before accessing it
+$selectedDate = isset($_SESSION['selected_date']) ? $_SESSION['selected_date'] : date('Y-m-d');
+$selectedTime = isset($_SESSION['selected_time']) ? $_SESSION['selected_time'] : '';
+
+// Save selected date and time to session for future use
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['selected_date'])) {
+        $_SESSION['selected_date'] = $_POST['selected_date'];
+    } else {
+        $_SESSION['selected_date'] = date('d-m-y');
+    }
+    if (isset($_POST['selected_time'])) {
+        $_SESSION['selected_time'] = $_POST['selected_time'];
+    } else {
+        $_SESSION['selected_time'] = date('H:i:s');
+    }
+} else{
+    $_SESSION['selected_date'] = date('d-m-y');
+    $_SESSION['selected_time'] = date('H:i:s');
 }
 
 function getComputerCount($conn, $roomID) {
@@ -28,15 +50,7 @@ if (!$buildingId) {
 
 $buildingId = intval($buildingId);
 
-// Save selected date and time to session for future use
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['selected_date'])) {
-        $_SESSION['selected_date'] = $_POST['selected_date'];
-    }
-    if (isset($_POST['selected_time'])) {
-        $_SESSION['selected_time'] = $_POST['selected_time'];
-    }
-}
+
 
 // Fetching available software
 $softwareOptions = "";
@@ -84,7 +98,8 @@ while ($software = mysqli_fetch_assoc($softwareResult)) {
             <h5>Select Filters</h5>
             <form method="POST" action="RoomSelection.php">
                 <input type="hidden" name="building_id" value="<?php echo htmlspecialchars($buildingId); ?>">
-                <input type="date" name="selected_date" class="form-control" value="<?php echo $_SESSION['selected_date'] ?? date('Y-m-d'); ?>">
+                <input type="date" name="selected_date" class="form-control" value="<?php echo $_SESSION['selected_date'] ?? 
+                date('Y-m-d'); ?>" min="<?php echo date('Y-m-d'); ?>">
                 <input type="number" name="min_seats" class="form-control mt-3" placeholder="Minimum Seats">
                 <input type="number" name="min_computers" class="form-control mt-3" placeholder="Minimum Computers">
                 <select name="software" class="form-control mt-3">
@@ -183,8 +198,9 @@ while ($software = mysqli_fetch_assoc($softwareResult)) {
         </div>
     </div>
 </div>
-<footer class="text-white text-center py-3 mt-5">
-    OpenBook &copy; 2025
-</footer>
+    <!-- Footer -->
+    <?php
+        display_footer();
+    ?>
 </body>
 </html>
