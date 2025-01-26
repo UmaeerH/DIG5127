e5153082cde221214c1b98dc510675871227db76
UmaeerH <umaeerh04@gmail.com>
2025-01-26 06:01:16 +0000

    Removing excess in aboutus.php

--- AboutUs.php
+++ AboutUs.php
@@ -37,12 +37,10 @@ include "resources/html_construct.php";
             <div class="col-md-6">
                 <div class="expandCard au-building-card h-100">
                     <div class="card-body text-center">
-                        <div class="au-circle-icon mx-auto mb-3">A</div>
                         <h5 class="card-title">Millenium Point Building</h5>
                         <img src="public_html/images/mp-exterior.jpeg" class="img-fluid my-3" alt="Millenium Point">
                         <h6>40+ Rooms Available</h6>
                         <p class="card-text">Located in the heart of the city centre, this lively and bright room would be a great choice for those looking for a private and quiet place to meet with their team.</p>
-                        <a href="BookRooms.php" class="btn btn-custom au-slow-hover">Book Now</a>
                     </div>
                 </div>
             </div>
@@ -51,12 +49,10 @@ include "resources/html_construct.php";
             <div class="col-md-6">
                 <div class="expandCard building-card h-100">
                     <div class="card-body text-center">
-                        <div class="circle-icon mx-auto mb-3">A</div>
                         <h5 class="card-title">Parkside Building</h5>
                         <img src="public_html/images/parkside-gallery.jpeg" class="img-fluid my-3" alt="Parkside Building">
                         <h6>30+ Rooms Available</h6>
                         <p class="card-text">The twin building of Millenium Point contains a lot of equipment for those studying or interested in the arts such as music and painting.</p>
-                        <a href="BookRooms.php" class="btn btn-custom au-slow-hover">Book Now</a>
                     </div>
                 </div>
             </div>
@@ -65,12 +61,10 @@ include "resources/html_construct.php";
             <div class="col-md-6">
                 <div class="expandCard building-card h-100">
                     <div class="card-body text-center">
-                        <div class="circle-icon mx-auto mb-3">A</div>
                         <h5 class="card-title">Curzon Building</h5>
                         <img src="public_html/images/curson-slider.jpeg" class="img-fluid my-3" alt="Curzon Building">
                         <h6>60+ Rooms Available</h6>
                         <p class="card-text">Located in the heart of the city centre, this lively and bright room would be a great choice for those looking for a private and quiet place to meet with their team.</p>
-                        <a href="BookRooms.php" class="btn btn-custom au-slow-hover">Book Now</a>
                     </div>
                 </div>
             </div>
@@ -79,12 +73,10 @@ include "resources/html_construct.php";
             <div class="col-md-6">
                 <div class="expandCard building-card h-100">
                     <div class="card-body text-center">
-                        <div class="circle-icon mx-auto mb-3">A</div>
                         <h5 class="card-title">STEAMhouse Building</h5>
                         <img src="public_html/images/steamhouse-exterior.jpeg" class="img-fluid my-3" alt="STEAMhouse Building">
                         <h6>50+ Rooms Available</h6>
                         <p class="card-text">Created with technology and innovation in mind, this cutting-edge building contains all the equipment a modern team would require to make the most out of their meetings.</p>
-                        <a href="BookRooms.php" class="btn btn-custom au-slow-hover">Book Now</a>
                     </div>
                 </div>
             </div>
@@ -92,16 +84,15 @@ include "resources/html_construct.php";
     </div>
 
     <section class="bg-light">
-        <div class="container text-center">
-            <h1>Require space for studying, meetings or events?</h1>
-            <p>We're here to support your academic and professional journey by providing a reliable and organized system that saves you time and keeps you focused on what matters most. </p>
-            <div class="card bg-light-custom text-black mt-4">
-                <div class="card-body">
-                    <p>Contact OpenBook for further enquiries</p>
-                    <a href="BookRooms.php" class="btn btn-outline-custom btn-lg au-slow-hover">Contact</a>
-                </div>
+    <div class="container text-center">
+        <h1>Require space for studying, meetings or events?</h1>
+        <p>We're here to support your academic and professional journey by providing a reliable and organized system that saves you time and keeps you focused on what matters most. </p>
+        <div class="card bg-light-custom text-black mt-4">
+            <div class="card-body">
+                <a href="SignUp.php" class="btn btn-outline-custom btn-lg au-slow-hover">Sign Up</a>
             </div>
         </div>
+    </div>
     </section>
 
     <!---- FOOTER ---->
